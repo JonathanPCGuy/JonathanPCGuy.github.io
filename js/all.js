cb8d8293a80145cbf8b09250dff224c9a10dda6f
Jonathan Lam <jlampcguy@gmail.com>
2016-01-15 12:18:06 -0600

    Fixed incorrect email

--- js/all.js
+++ js/all.js
@@ -22,7 +22,7 @@ jlamdata.contact = {
         { 'title': 'LinkedIn', 'logo': 'images/contact/In-2C-66px-TM.png', 'url': 'https://www.linkedin.com/in/jonathan-lam-2332064' },
         { 'title': 'GitHub', 'logo': 'images/contact/GitHub-Mark-64px.png', 'url': 'https://www.github.com/JonathanPCGuy' },
         { 'title': 'Twitter', 'logo': 'images/contact/TwitterLogo_color.png', 'url': 'https://www.twitter.com/JonathanPCGuy' },
-        { 'title': 'E-mail', 'logo': 'images/simpleicon/mail59.svg', 'url': 'mailto:jonathanpcguy@gmail.com' }
+        { 'title': 'E-mail', 'logo': 'images/simpleicon/mail59.svg', 'url': 'mailto:jlampcguy@gmail.com' }
     ]
 };
 // i won't list all here; i will add link to see more
